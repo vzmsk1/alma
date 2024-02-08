623aeb58fb444b00e697333eafa037465a6fc105
ukik0 <ctaricc@mail.ru>
2024-02-08 10:58:34 +0300

    feat: intro services template section

--- src/js/dev/ukik0.js
+++ src/js/dev/ukik0.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-   if (document.querySelector('.knowledge')) {
-       const filters = document.querySelector('.knowledge__filters-list');
+    if (document.querySelector('.knowledge')) {
+        const filters = document.querySelector('.knowledge__filters-list');
 
-       window.addEventListener('resize', () => {
-           const value = window.innerWidth > 768 ? 6 : 10;
+        window.addEventListener('resize', () => {
+            const value = window.innerWidth > 768 ? 6 : 10;
 
-           filters.style.height = `${value}rem`;
-           filters.dataset.showmoreContent = value;
-       });
-   }
+            filters.style.height = `${value}rem`;
+            filters.dataset.showmoreContent = value;
+        });
+    }
 });
