1cb90a0d00f70faa12db4970a23b4817f7e709f1
ukik0 <ctaricc@mail.ru>
2024-02-17 17:27:59 +0300

    refactor: contacts page

--- src/js/dev/ukik0.js
+++ src/js/dev/ukik0.js
@@ -2,8 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector('.knowledge')) {
         const filters = document.querySelector('.knowledge__filters-list');
 
+        if (window.innerWidth < 768) {
+            filters.style.height = `9rem`;
+            filters.dataset.showmoreContent = 9;
+        }
+
         window.addEventListener('resize', () => {
-            const value = window.innerWidth > 768 ? 6 : 10;
+            const value = window.innerWidth > 768 ? 6 : 9;
 
             filters.style.height = `${value}rem`;
             filters.dataset.showmoreContent = value;
